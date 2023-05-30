f107d08a73ba36b3a5658e72e768b142b79be79e
JasonLuis <jason20luis@gmail.com>
2023-05-29 22:23:44 -0300

    chore: create all exceptions

--- src/main/kotlin/me/dio/credit/application/system/dto/CustomerDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CustomerDto.kt
@@ -1,18 +1,25 @@
 package me.dio.credit.application.system.dto
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.credit.application.system.entity.Address
 import me.dio.credit.application.system.entity.Customer
+import org.hibernate.validator.constraints.br.CPF
 import java.math.BigDecimal
 
 data class CustomerDto(
-        val firstName: String,
-        val lastName: String,
-        val cpf: String,
-        val income: BigDecimal,
+        @field:NotEmpty(message = "First Name is required") val firstName: String,
+        @field:NotEmpty(message = "Last Name is required") val lastName: String,
+        @field:NotEmpty(message = "CPF is required")
+        @field:CPF(message = "This invalid CPF") val cpf: String,
+        @field:NotNull(message = "Invald income") val income: BigDecimal,
+        @field:NotEmpty(message = "E-mail is required")
+        @field:Email(message = "This E-mail is invalid\n")
         val email: String,
-        val password: String,
-        val zipCode: String,
-        val street: String
+        @field:NotEmpty(message = "Password is required") val password: String,
+        @field:NotEmpty(message = "Zip Code is required") val zipCode: String,
+        @field:NotEmpty(message = "Street is required") val street: String
 ) {
 
     fun toEntity(): Customer = Customer(
--- src/main/kotlin/me/dio/credit/application/system/dto/CustomerUpdateDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CustomerUpdateDto.kt
@@ -1,14 +1,16 @@
 package me.dio.credit.application.system.dto
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.credit.application.system.entity.Customer
 import java.math.BigDecimal
 
-data class CustomerUpdateDto (
-        val firstName: String,
-        val lastName: String,
-        val income: BigDecimal,
-        val zipCode: String,
-        val street: String
+data class CustomerUpdateDto(
+        @field:NotEmpty(message = "First Name is required") val firstName: String,
+        @field:NotEmpty(message = "Last Name is required") val lastName: String,
+        @field:NotNull(message = "Invald income") val income: BigDecimal,
+        @field:NotEmpty(message = "Zip Code is required") val zipCode: String,
+        @field:NotEmpty(message = "Street is required") val street: String
 ) {
 
     fun toEntity(customer: Customer): Customer {
